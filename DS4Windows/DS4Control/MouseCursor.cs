c13b43045a01e8e15826ef5d5b2ec529c94cc40d
Rajko Stojadinovic <admin@rajko.info>
2017-07-07 11:18:54 +0200

    Simplify gyro mouse so that it actually moves now (why would you "smooth" angular velocity values ?) Still don't know what the actual intended result of gyro mouse is

--- DS4Windows/DS4Control/MouseCursor.cs
+++ DS4Windows/DS4Control/MouseCursor.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace DS4Windows
 {
     class MouseCursor
@@ -18,21 +20,6 @@ namespace DS4Windows
         private Direction horizontalDirection = Direction.Neutral, verticalDirection = Direction.Neutral;
         private Direction hDirection = Direction.Neutral, vDirection = Direction.Neutral;
 
-        private double GYRO_MOUSE_COEFFICIENT = 0.0095;
-        private int GYRO_MOUSE_DEADZONE = 12;
-        private double GYRO_MOUSE_OFFSET = 0.1463;
-        private double GYRO_SMOOTH_MOUSE_OFFSET = 0.14698;
-
-        private const int SMOOTH_BUFFER_LEN = 3;
-        private double[] xSmoothBuffer = new double[SMOOTH_BUFFER_LEN];
-        private double[] ySmoothBuffer = new double[SMOOTH_BUFFER_LEN];
-        private int smoothBufferTail = 0;
-
-        double coefficient = 0.0;
-        double verticalScale = 0.0;
-        bool gyroSmooth = false;
-        //double gyroSmoothWeight = 0.0;
-
         public virtual void sixaxisMoved(SixAxisEventArgs arg)
         {
             double deltaX = 0, deltaY = 0;
@@ -41,145 +28,35 @@ namespace DS4Windows
                 ulong timestampDeltaUs = arg.sixAxis.timestampUs - arg.sixAxis.previous.timestampUs;
                 double timestampDelta = (double)(timestampDeltaUs) * 0.000001;
 
-                deltaX = arg.sixAxis.gyroRoll * timestampDelta; //according to sony convention, Roll is positive "to the right"
-                deltaY = arg.sixAxis.gyroPitch * timestampDelta; //according to sony convention, Pitch is positive "towards you" (down)
+                var DS4_GYRO_DEADZONE = 0.707;
+
+                if (Math.Abs(arg.sixAxis.gyroRoll) < DS4_GYRO_DEADZONE)
+                    deltaX = 0;
+                else
+                    deltaX = arg.sixAxis.gyroRoll * timestampDelta; //according to sony convention, Roll is positive "to the right"
+
+                if (Math.Abs(arg.sixAxis.gyroPitch) < DS4_GYRO_DEADZONE)
+                    deltaY = 0;
+                else
+                    deltaY = arg.sixAxis.gyroPitch * timestampDelta; //according to sony convention, Pitch is positive "towards you" (down)
             }
 
-            gyroSmooth = Global.getGyroSmoothing(deviceNumber);
-            double gyroSmoothWeight = 0.0;
-
-            coefficient = (Global.getGyroSensitivity(deviceNumber) * 0.01) * GYRO_MOUSE_COEFFICIENT;
-            double offset = GYRO_MOUSE_OFFSET;
-            if (gyroSmooth)
-            {
-                gyroSmoothWeight = Global.getGyroSmoothingWeight(deviceNumber);
-                if (gyroSmoothWeight > 0.0)
-                {
-                    offset = GYRO_SMOOTH_MOUSE_OFFSET;
-                }
-            }
-
-            double tempAngle = System.Math.Atan2(-deltaY, deltaX);
-            double normX = System.Math.Abs(System.Math.Cos(tempAngle));
-            double normY = System.Math.Abs(System.Math.Sin(tempAngle));
-            int signX = System.Math.Sign(deltaX);
-            int signY = System.Math.Sign(deltaY);
-
-            if ((hRemainder > 0) != (deltaX > 0))
-            {
-                hRemainder = 0.0;
-            }
-
-            if ((vRemainder > 0) != (deltaY > 0))
-            {
-                vRemainder = 0.0;
-            }
-
-            int deadzone = GYRO_MOUSE_DEADZONE;
-            //int deadzone = 0;
-            int deadzoneX = (int)System.Math.Abs(normX * deadzone);
-            int deadzoneY = (int)System.Math.Abs(normY * deadzone);
-
-            if (System.Math.Abs(deltaX) > deadzoneX)
-            {
-                deltaX -= signX * deadzoneX;
-            }
-            else
-            {
-                deltaX = 0;
-            }
-
-            if (System.Math.Abs(deltaY) > deadzoneY)
-            {
-                deltaY -= signY * deadzoneY;
-            }
-            else
-            {
-                deltaY = 0;
-            }
-
-            double xMotion = deltaX != 0 ? coefficient * deltaX + (normX * (offset * signX)) : 0;
-            int xAction = 0;
-            if (xMotion != 0.0)
-            {
-                xMotion += hRemainder;
-                //xAction = (int)xMotion;
-                //hRemainder = xMotion - xAction;
-            }
-            else
-            {
-                //hRemainder = 0.0;
-            }
-
-            //hRemainder -= (int)hRemainder;
-            verticalScale = Global.getGyroSensVerticalScale(deviceNumber) * 0.01;
-            double yMotion = deltaY != 0 ? (coefficient * verticalScale) * deltaY + (normY * (offset * signY)) : 0;
-            int yAction = 0;
-            if (yMotion != 0.0)
-            {
-                yMotion += vRemainder;
-                //yAction = (int)yMotion;
-                //vRemainder = yMotion - yAction;
-            }
-            else
-            {
-                //vRemainder = 0.0;
-            }
-
-            if (gyroSmooth)
-            {
-                int iIndex = smoothBufferTail % SMOOTH_BUFFER_LEN;
-                xSmoothBuffer[iIndex] = xMotion;
-                ySmoothBuffer[iIndex] = yMotion;
-                smoothBufferTail = iIndex + 1;
-
-                double currentWeight = 1.0;
-                double finalWeight = 0.0;
-                double x_out = 0.0, y_out = 0.0;
-                for (int i = 0; i < SMOOTH_BUFFER_LEN; i++)
-                {
-                    int idx = System.Math.Abs(smoothBufferTail - i - 1) % SMOOTH_BUFFER_LEN;
-                    x_out += xSmoothBuffer[idx] * currentWeight;
-                    y_out += ySmoothBuffer[idx] * currentWeight;
-                    finalWeight += currentWeight;
-                    currentWeight *= gyroSmoothWeight;
-                }
-
-                x_out /= finalWeight;
-                xMotion = x_out;
-                y_out /= finalWeight;
-                yMotion = y_out;
-            }
-
-            if (xMotion != 0.0)
-            {
-                xAction = (int)xMotion;
-                hRemainder = xMotion - xAction;
-            }
-            else
-            {
-                hRemainder = 0.0;
-            }
-
-            if (yMotion != 0.0)
-            {
-                yAction = (int)yMotion;
-                vRemainder = yMotion - yAction;
-            }
-            else
-            {
-                vRemainder = 0.0;
-            }
-
-            //vRemainder -= (int)vRemainder;
-
-            int gyroInvert = Global.getGyroInvert(deviceNumber);
-            if (gyroInvert == 2 || gyroInvert == 3)
+            double coefficient = Global.GyroSensitivity[deviceNumber] / 100f;
+            //Collect rounding errors instead of losing motion.
+            double xMotion = coefficient * deltaX;
+            xMotion += hRemainder;
+            int xAction = (int)xMotion;
+            hRemainder += xMotion - xAction;
+            hRemainder -= (int)hRemainder;
+            double yMotion = coefficient * deltaY;
+            yMotion += vRemainder;
+            int yAction = (int)yMotion;
+            vRemainder += yMotion - yAction;
+            vRemainder -= (int)vRemainder;
+            if (Global.GyroInvert[deviceNumber] == 2 || Global.GyroInvert[deviceNumber] == 3)
                 xAction *= -1;
-
-            if (gyroInvert == 1 || gyroInvert == 3)
+            if (Global.GyroInvert[deviceNumber] == 1 || Global.GyroInvert[deviceNumber] == 3)
                 yAction *= -1;
-
             if (yAction != 0 || xAction != 0)
                 InputMethods.MoveCursorBy(xAction, yAction);
 
@@ -190,10 +67,6 @@ namespace DS4Windows
         public void mouseRemainderReset()
         {
             hRemainder = vRemainder = 0.0;
-            int iIndex = smoothBufferTail % SMOOTH_BUFFER_LEN;
-            xSmoothBuffer[iIndex] = 0.0;
-            ySmoothBuffer[iIndex] = 0.0;
-            smoothBufferTail = iIndex + 1;
         }
 
         public void touchesBegan(TouchpadEventArgs arg)
